a43290a2b82d38c4ee0800577a1aad0b8f0ac5d9
Takumi Hashimoto <39505885+takumi-hash@users.noreply.github.com>
2018-06-19 22:08:30 +0900

    card design changed

--- resources/views/books/books.blade.php
+++ resources/views/books/books.blade.php
@@ -1,25 +1,29 @@
 @if ($books)
     <div class="row">
         @foreach ($books as $book)
-            <div class="card col-md-2 col-sm-4 bg-dark">
-                <img class="card-img-top" src="{{ $book->image_url }}" alt="{{ $book->title }}">
-                <div class="card-body text-light">
-                    @if ($book->id)
-                    <p class="card-title"><a href="{{ route('books.show', $book->id) }}">{{ $book->title }}</a></p>
-                    @else
-                    <p class="card-text">{{ $book->title }}</p>
-                    @endif
-                    <p class="card-text text-white-50">{{ $book->author }}</p>
-                    <hr>
-                    <small class="text-muted">{{ $book->publisher }}</small>
-                    <small class="text-muted">{{ $book->isbn }}</small>
+            <div class="col-md-4 col-sm-4 bg-light d-flex align-items-center p-1">
+                <div class="col-6">
+                    <img class="book-img" src="{{ $book->image_url }}" alt="{{ $book->title }}">
                 </div>
-                <div class="card-body">
-                    <a href="{{ $book->url }}" class="btn btn-outline-primary">Buy</a>
-                    @if (Auth::check())
-                        @include('books.want_button', ['book' => $book])
-                    @endif
-                    <a href="#" class="btn btn-outline-info">Have</a>
+                <div class="col-6">
+                    <div class="text-dark">
+                        @if ($book->id)
+                        <p class=""><a href="{{ route('books.show', $book->id) }}">{{ $book->title }}</a></p>
+                        @else
+                        <p class="">{{ $book->title }}</p>
+                        @endif
+                        <p class="text-white-50">{{ $book->author }}</p>
+                        <hr>
+                        <small class="text-muted">{{ $book->publisher }}</small>
+                        <small class="text-muted">{{ $book->isbn }}</small>
+                    </div>
+                    <div class="">
+                        <a href="{{ $book->url }}" class="btn btn-outline-primary">Buy</a>
+                        @if (Auth::check())
+                            @include('books.want_button', ['book' => $book])
+                        @endif
+                        <a href="#" class="btn btn-outline-info">Have</a>
+                    </div>
                 </div>
             </div>
         @endforeach
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,12 +14,8 @@
 
         <title>{{ config('app.name', 'ドクショカ') }}</title>
 
-    @if(app('env')=='local')
-    <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
-    @elseif(app('env')=='production')
-    <link rel="stylesheet" href="{{ secure_asset('/css/style.css') }}">
-    @endif
-        <link rel="stylesheet" href="{{ secure_asset('/css/style.css') }}" media="all" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="/css/style.css">
+        {{--<link rel="stylesheet" href="{{ secure_asset('/css/style.css') }}" media="all" rel="stylesheet" type="text/css">--}}
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
