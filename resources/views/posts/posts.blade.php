99d1a5db63a9c53606f9313ca5025d57ad37451d
takumi.hashimoto <ec2-user@ip-172-31-14-114.us-east-2.compute.internal>
2018-06-22 07:48:51 +0000

    post head revised

--- resources/views/posts/posts.blade.php
+++ resources/views/posts/posts.blade.php
@@ -3,25 +3,29 @@
     <?php $user = $post->user; ?>
     <div class="card my-3">
         <div class="card-body p-4">
-            <div class="row">
-                <div class="col-md-1 col-4">
+            <div class="row py-4">
+                <div class="col-md-1 offset-md-1 col-4">
                     <img class="rounded-circle align-middle" src="/images/user{{ $user->id }}.jpg" alt="">
                 </div>
-                <div class="col-md-10 col-7">
+                <div class="col-md-8 col-7">
                     <span class="">{!! link_to_route('users.show', $user->name, ['id' => $user->id]) !!}</span>
                     <span class="text-muted ml-3">{{ $post->created_at }}</span>
                 </div>
-                <div class="col-1 offset-10">
+                <div class="col-md-1">
                     @if(Auth::check())
                         @include('posts.favorite_button', ['post' => $post])
                     @endif
                 </div>
+            </div>
+            <div class="row">
                 <div class="col-md-10 offset-md-1">
                     <h3 class="text-dark py-3 post-title">{!! nl2br(e($post->title)) !!}</h3>
                     <hr>
                     <p class="post-content">{!! nl2br(e($post->content)) !!}</p>
                 </div>
-                <div class="col-1 offset-10">
+            </div>
+            <div class="row">
+                <div class="col-1 offset-11">
                     @if (Auth::id() == $post->user_id)
                         {!! Form::open(['route' => ['posts.destroy', $post->id], 'method' => 'delete', 'class'=>'text-right']) !!}
                             {!! Form::button('<i class="far fa-trash-alt"></i>', ['type'=> 'submit', 'class' => 'btn btn-link text-secondary']) !!}
@@ -29,6 +33,7 @@
                     @endif
                 </div>
             </div>
+
         </div>
     </div>
 @endforeach
