5d5b8d8aea8bfee29b6ad786fb12239979625191
takumi.hashimoto <ec2-user@ip-172-31-14-114.us-east-2.compute.internal>
2018-06-22 07:50:53 +0000

    top heading changed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 @section('cover')
     <div class="jumbotron jumbotron-fluid jumbotron-extend">
         <div class="container text-center">
-            <h1 class="display-4">素敵な本と出会う場所</h1>
+            <h1 class="">素敵な本と出会う場所</h1>
             <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
     @if (!Auth::check())
         <a href="{{ route('register') }}" class="btn btn-primary btn-lg mt-5">ドクショカを始める</a>
