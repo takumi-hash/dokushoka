0d5e5cefb89120567c6f7f2d2a5da0e6423a8069
takumi.hashimoto <ec2-user@ip-172-31-14-114.us-east-2.compute.internal>
2018-06-22 07:26:36 +0000

    mobile view profile

--- resources/views/posts/posts.blade.php
+++ resources/views/posts/posts.blade.php
@@ -2,7 +2,7 @@
 @foreach ($posts as $post)
     <?php $user = $post->user; ?>
     <div class="card my-3">
-        <div class="card-body p-5">
+        <div class="card-body p-2">
             <div class="row">
                 <div class="col-md-1 col-4">
                     <img class="rounded-circle align-middle" src="/images/user{{ $user->id }}.jpg" alt="">
@@ -11,17 +11,17 @@
                     <span class="">{!! link_to_route('users.show', $user->name, ['id' => $user->id]) !!}</span>
                     <span class="text-muted ml-3">{{ $post->created_at }}</span>
                 </div>
-                <div class="col-1">
+                <div class="col-1 offset-10">
                     @if(Auth::check())
                         @include('posts.favorite_button', ['post' => $post])
                     @endif
                 </div>
-                <div class="col-lg-10 offset-lg-1">
+                <div class="col-md-10 offset-md-1">
                     <h3 class="text-dark py-3 post-title">{!! nl2br(e($post->title)) !!}</h3>
                     <hr>
                     <p class="post-content">{!! nl2br(e($post->content)) !!}</p>
                 </div>
-                <div class="col-1 offset-11">
+                <div class="col-1 offset-10">
                     @if (Auth::id() == $post->user_id)
                         {!! Form::open(['route' => ['posts.destroy', $post->id], 'method' => 'delete', 'class'=>'text-right']) !!}
                             {!! Form::button('<i class="far fa-trash-alt"></i>', ['type'=> 'submit', 'class' => 'btn btn-link text-secondary']) !!}
