9509e184cb8653805d6b30d99ea7258e3f5ea5ae
takumi.hashimoto <ec2-user@ip-172-31-14-114.us-east-2.compute.internal>
2018-06-22 07:19:45 +0000

    mobile view profile

--- resources/views/posts/posts.blade.php
+++ resources/views/posts/posts.blade.php
@@ -4,10 +4,10 @@
     <div class="card my-3">
         <div class="card-body p-5">
             <div class="row">
-                <div class="col-lg-1 col-sm-4">
+                <div class="col-md-1 col-4">
                     <img class="rounded-circle align-middle" src="/images/user{{ $user->id }}.jpg" alt="">
                 </div>
-                <div class="col-lg-10 col-sm-7">
+                <div class="col-md-10 col-7">
                     <span class="">{!! link_to_route('users.show', $user->name, ['id' => $user->id]) !!}</span>
                     <span class="text-muted ml-3">{{ $post->created_at }}</span>
                 </div>
