039ec9d11a4da49714a9828ac9a6324712924198
takumi.hashimoto <ec2-user@ip-172-31-14-114.us-east-2.compute.internal>
2018-06-22 08:25:59 +0000

    css lg

--- resources/views/posts/posts.blade.php
+++ resources/views/posts/posts.blade.php
@@ -4,21 +4,21 @@
     <div class="card my-3">
         <div class="card-body p-4">
             <div class="row py-4">
-                <div class="col-md-1 offset-md-1">
+                <div class="col-lg-1 offset-lg-1">
                     <img class="rounded-circle align-middle" src="/images/user{{ $user->id }}.jpg" alt="">
                 </div>
-                <div class="col-md-8">
+                <div class="col-lg-8">
                     <span class="">{!! link_to_route('users.show', $user->name, ['id' => $user->id]) !!}</span>
                     <span class="text-muted ml-3">{{ $post->created_at }}</span>
                 </div>
-                <div class="col-md-1">
+                <div class="col-lg-1">
                     @if(Auth::check())
                         @include('posts.favorite_button', ['post' => $post])
                     @endif
                 </div>
             </div>
             <div class="row">
-                <div class="col-md-10 offset-md-1">
+                <div class="col-lg-10 offset-lg-1">
                     <h3 class="text-dark py-3 post-title">{!! nl2br(e($post->title)) !!}</h3>
                     <hr>
                     <p class="post-content">{!! nl2br(e($post->content)) !!}</p>
