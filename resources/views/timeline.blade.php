7dd8b2b650a7dc59fcc5d4c755f87a21b408dafc
Takumi Hashimoto <39505885+takumi-hash@users.noreply.github.com>
2018-06-24 13:15:41 +0900

    jumbotron button route

--- resources/views/timeline.blade.php
+++ resources/views/timeline.blade.php
@@ -11,7 +11,7 @@
         <div class="center jumbotron">
             <div class="text-center">
                 <h1>please log in</h1>
-                {!! link_to_route('signup.get', 'Sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}
+                {!! link_to_route('register', 'Sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}
             </div>
         </div>
     @endif
