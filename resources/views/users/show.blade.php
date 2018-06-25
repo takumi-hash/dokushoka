4968bfa3b55d434846fd9d45bb655bbdf35fe11f
takumi.hashimoto <ec2-user@ip-172-31-14-114.us-east-2.compute.internal>
2018-06-25 05:06:15 +0000

    profile photo removed

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="p-5 bg-dark text-center">
         <div class="mt-5 mb-3">
-            <img src="/images/user{{ $user->id }}.jpg" alt="" class="rounded-circle user-photo">
+            <img src="/images/user{{$user->id}}.jpg" alt="" class="rounded-circle user-photo">
         </div>
         <h2 class="font-weight-normal text-light">{{ $user->name }}</h2>
         <p class="lead mt-3 text-light"><i class="fas fa-quote-left text-white-50 mr-3"></i>{{ $user->bio }}<i class="fas fa-quote-right text-white-50 ml-3"></i></p>
