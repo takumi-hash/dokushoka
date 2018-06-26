1459ce0c2a69e1f099050d8ba74732bfb1d1db3f
takumi.hashimoto <ec2-user@ip-172-31-14-114.us-east-2.compute.internal>
2018-06-26 23:45:00 +0000

    book name on create page and write button auth check added

--- resources/views/books/books.blade.php
+++ resources/views/books/books.blade.php
@@ -29,8 +29,10 @@
                             @endif
                             <a href="{{ $book->url }}" class="btn btn-link text-info"><i class="fas fa-shopping-cart"></i></a>
                         </div>
-                        @if (Auth::user()->is_wanting($book->id) || Auth::user()->is_having($book->id))
-                            {!! link_to_route('posts.create', '書評を書く', ['book_id' => $book->id],  ['class' => 'btn btn-block btn-primary']) !!}
+                        @if(Auth::check())
+                            @if (Auth::user()->is_wanting($book->id) || Auth::user()->is_having($book->id))
+                                {!! link_to_route('posts.create', '書評を書く', ['book_id' => $book->id],  ['class' => 'btn btn-block btn-primary']) !!}
+                            @endif
                         @endif
                     </div>
                 </div>
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,8 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="bg-dark py-5" id="form-bg">
+<div class="bg-dark pb-3 pt-2" id="form-bg">
     <div class="container">
+        <div class="text-white-50 my-3">『{{ $book->title }}』（{{ $book->publisher }}）／ {{ $book->author }}</div>
         @if (Auth::id() == $user->id)
         {!! Form::open(['route' => 'posts.store']) !!}
             <div class="form-group p-2" id="review-form-group">
@@ -10,7 +11,6 @@
                 {!! Form::textarea('title', old('title'), ['class' => 'form-control pt-3 font-weight-bold', 'id'=>'form-title', 'placeholder'=>'Title','rows' => '1']) !!}
                 <hr>
                 {!! Form::textarea('content', old('content'), ['class' => 'form-control', 'id'=>'form-content', 'placeholder'=>'Markdown記法を使えます。']) !!}
-                You're writing on {{ $book->title }}.
                 {!! Form::submit('投稿する', ['class' => 'btn btn-primary btn-block', 'id' => 'form-button']) !!}
             </div>
             {!! Form::close() !!}
